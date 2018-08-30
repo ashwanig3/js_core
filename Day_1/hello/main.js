f1bf935e54675bde9ebb4282bf8a112ea4dd5e49
ashwanig3 <goswami17829@gmail.com>
2018-08-31 00:20:50 +0530

    Added_day_1 folder

---
+++ Day_1/hello/main.js
@@ -0,0 +1,23 @@
+
+// ==================================
+// first program
+// ==============================
+// var name= prompt("your name");
+// if(name==""){
+// 	name="unknown";
+// }
+// var age=21;
+// var college=true;
+// alert(`hello ${name} your age is ${age}.college= ${college}`);
+
+
+// for(var i=1; i<20; i=i+1){
+// 	console.log(i)
+// }
+
+function add(a,b){
+	return a+b;
+}
+function multiply(a,b){
+	return a*b;
+}
