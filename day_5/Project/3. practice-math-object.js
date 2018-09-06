f464c39ab04a7906a5d71fa9a01eebfe323ea7fc
ashwanig3 <goswami17829@gmail.com>
2018-09-06 22:47:49 +0530

    day_5_practice

--- day_5/Project/3. practice-math-object.js
+++ day_5/Project/3. practice-math-object.js
@@ -6,6 +6,11 @@ Example:
 rand(max,min)
 console.log(rand(20,1)); //15
 console.log(rand(1,10)); //8
+ 
+ function randInteger(max,min){
+    return Math.floor(Math.random() * max) + min;
+	}
+
 
 
 2.
@@ -15,6 +20,10 @@ console.log(decimals(2.100212, 2)); //2.10
 console.log(decimals(2.100212, 3)); //2.100
 console.log(decimals(2100, 2)); //2100.00
 
+	function decimals(number,tofixed){
+    return Number.parseFloat(number).toFixed(tofixed);
+	}
+	console.log(decimals(5.00043 , 2));
 
 3. Write a JavaScript function to find the highest value in an array.
 
@@ -22,3 +31,17 @@ Example:
 console.log(maxNumber([12,34,56,1])); //56
 console.log(maxNumber([-12,-34,0,-56,-1])); //0
 
+
+function max(arr){
+    var max = arr[0];
+    
+    for(var i=1; i< arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+arr = [24,335,74,35,33];
+console.log(max(arr));
